873b059d019641eb07dd955a64dd355a6983a5e3
Witless <witlesshydra@outlook.com>
2024-08-30 14:51:05 +0200

    Added endpoint to check subsequent week times

---
+++ src/routes/calendar/next-week/+page.js
@@ -0,0 +1,30 @@
+import moment from 'moment';
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
+import { firestore } from '$lib/firebaseClient.js';
+
+let timesData = {
+	Monday: [],
+	Tuesday: [],
+	Wednesday: [],
+	Thursday: [],
+	Friday: [],
+	Saturday: [],
+	Sunday: []
+};
+export async function load() {
+	const startOfWeek = moment().startOf('isoWeek').add(7, 'days');
+	const endOfWeek = moment().endOf('isoWeek').add(7, 'days');
+	const timesCollection = collection(firestore, 'times');
+	const timesQuery = query(timesCollection,  where('visible', '==', true), orderBy('initial'));
+	const orderedTimes = await getDocs(timesQuery);
+	orderedTimes.docs.map((doc) => {
+		const data = doc.data();
+		const initial = moment(data.initial.toDate());
+		if (initial > startOfWeek && initial < endOfWeek) {
+			data.initial = initial;
+			data.ending = moment(data.ending.toDate());
+			timesData[initial.format('dddd')].push(data);
+		}
+	});
+	return timesData;
+}
---
+++ src/routes/calendar/next-week/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+
+	import moment from 'moment';
+	import { onMount } from 'svelte';
+	export let data;
+
+	let inIFrame = false;
+
+	onMount(() => {
+		inIFrame = window.self !== window.top;
+	});
+
+	const getCurrentWeekDatesFormatted = () => {
+		const startOfWeek =  moment().startOf('isoweek').add(7, 'days');
+		let dates = [];
+
+		for(let i = 0; i < 7; i++){
+			dates.push(moment(startOfWeek).add(i, 'days').format("dddd DD[/]MM"))
+		}
+
+		return dates;
+
+	}
+
+	const thisWeek = getCurrentWeekDatesFormatted();
+</script>
+
+<div class="flex flex-col items-center p-4 ${inIFrame ? 'bg-white' : ''}">
+	<button class="btn btn-wide font-bold btn-info text-white mb-5" style="background-color: #00aeef; border-color: #00aeef"> OFFICE OPEN HOURS</button>
+
+	<div class="overflow-x-auto">
+		<table class="table">
+			<tbody>
+				{#each thisWeek as day}
+					<tr class="max-[600px]:border-b max-[600px]:border-gray-700 ${inIFrame ? 'border-b border-gray-700' : ''}" style="border-color: #f47b20">
+						<th>
+							<div class="btn btn-lg btn-primary w-32 md:w-56 text-white" style="background-color: #f47b20; border-color: #f47b20">
+								{day}
+							</div>
+						</th>
+						{#if data[day.split(' ')[0]].length}
+							<div class="flex flex-col">
+								{#each data[day.split(' ')[0]] as time}
+									<th>
+										<div class="btn btn-lg btn-success md:w-87 text-white w-48" style="background-color: #7ac143; border-color: #7ac143">
+											<p>{time.initial.format('HH[:]mm')} to {time.ending.format('HH[:]mm')}</p>
+										</div>
+									</th>
+								{/each}
+							</div>
+							{:else}
+							<th>
+								<div class="btn btn-lg btn-success md:w-87 text-white w-48" style="background-color: rgba(122,193,67,0.74); border-color: rgba(122,193,67,0.74)">
+									<p>🚫 Office closed</p>
+								</div>
+							</th>
+						{/if}
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
